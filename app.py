a56713039d0970563567682aca56170fe01a4114
Johnny-Morgan <jwjmorgan@gmail.com>
2021-04-27 10:39:10 +0000

    Add registration functionality

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists('env.py'):
     import env
 
@@ -16,12 +17,38 @@ app.secret_key = os.environ.get('SECRET_KEY')
 
 mongo = PyMongo(app)
 
-# test route
+
 @app.route('/')
 @app.route('/get_hikes')
 def get_hikes():
     hikes = mongo.db.hikes.find()
     return render_template('hikes.html', hikes=hikes)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # check if username already exists in the database
+
+        existing_user = mongo.db.users.find_one(
+            {'username': request.form.get('username')})
+
+        if existing_user:
+            flash('Username already exists')
+            return redirect(url_for("register"))
+
+        register = {'username': request.form.get('username'),
+                    'password': generate_password_hash(
+                        request.form.get('password'))
+                    }
+
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session['user'] = request.form.get('username')
+        flash('Registration Successful!')
+        return redirect(url_for('get_hikes'))
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
