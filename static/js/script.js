6d326cfa5f723017f793e3ee84fdc0da3da343ea
Johnny-Morgan <jwjmorgan@gmail.com>
2021-04-28 15:42:33 +0000

    Add JavaScript to display datepicker

--- static/js/script.js
+++ static/js/script.js
@@ -26,3 +26,18 @@ $(window).on('resize', function () {
         $('.table').removeClass('table-sm');
     }
 });
+
+
+// Datepicker
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
+
+today = yyyy + '-' + mm + '-' + dd;
+$('#picker').datetimepicker({
+    timepicker: false,
+    datepicker: true,
+    format: 'd-M-Y',
+    value: today,
+});
