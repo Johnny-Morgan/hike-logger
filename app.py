9be094fad2fc0eb52fe81d93ed968481c1ce61f5
Johnny-Morgan <jwjmorgan@gmail.com>
2021-05-05 00:01:08 +0000

    Create home page

--- app.py
+++ app.py
@@ -360,6 +360,13 @@ def delete_time(time_id):
     return render_template('delete_time.html', time=time)
 
 
+@app.route('/home')
+def home():
+    # get the 4 most recently add hikes form db
+    latest_hikes = list(mongo.db.hikes.find())[-4:]
+    return render_template('home.html', latest_hikes=latest_hikes)
+ 
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
