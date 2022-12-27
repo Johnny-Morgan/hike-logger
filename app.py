948cc6b7833d99735ad511384ff6039cb89cc678
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2022-12-27 22:49:52 +0000

    Enable debug mode

--- app.py
+++ app.py
@@ -489,4 +489,4 @@ def delete_time(time_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=False)
+            debug=True)
